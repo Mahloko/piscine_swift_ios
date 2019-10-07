33a34214f73f5256864510e9654f69a6e5bbeebe
Mpho MAHLOKO <mmahloko@c5r1s10.wethinkcode.co.za>
2019-10-07 15:43:03 +0200

    hello world functionality

---
+++ d00/ex00/helloWorld/helloWorld/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  helloWorld
+//
+//  Created by Mpho MAHLOKO on 2019/10/07.
+//  Copyright © 2019 Mpho MAHLOKO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func clickMe(_ sender: Any) {
+        print("Hello World !")
+    }
+    
+}
+
