b63d8f1c5415605471280aed266174109deb3da6
Mpho MAHLOKO <mmahloko@c5r1s10.wethinkcode.co.za>
2019-10-07 18:50:16 +0200

    ex01 works

--- d00/ex01/supersizeMe/supersizeMe/ViewController.swift
+++ d00/ex01/supersizeMe/supersizeMe/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  helloWorld
+//  supersizeMe
 //
 //  Created by Mpho MAHLOKO on 2019/10/07.
 //  Copyright © 2019 Mpho MAHLOKO. All rights reserved.
@@ -9,14 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var displayMsg: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        return ;
     }
-
+        
     @IBAction func clickMe(_ sender: Any) {
+        displayMsg.text = "You clicked the button !";
         print("Hello World !")
+        return ;
     }
-    
 }
+
--- d00/ex02/moarButtons!/moarButtons!/ViewController.swift
+++ d00/ex02/moarButtons!/moarButtons!/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  helloWorld
+//  moarButtons!
 //
 //  Created by Mpho MAHLOKO on 2019/10/07.
 //  Copyright © 2019 Mpho MAHLOKO. All rights reserved.
@@ -15,8 +15,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func clickMe(_ sender: Any) {
-        print("Hello World !")
-    }
-    
+
 }
+
