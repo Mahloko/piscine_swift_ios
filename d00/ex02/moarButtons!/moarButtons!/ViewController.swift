adf9a3eb349a1425689740914ea89aa4a819509c
Mpho MAHLOKO <mmahloko@c5r1s10.wethinkcode.co.za>
2019-10-08 11:57:50 +0200

    ex02

--- d00/ex02/moarButtons!/moarButtons!/ViewController.swift
+++ d00/ex02/moarButtons!/moarButtons!/ViewController.swift
@@ -15,6 +15,63 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var displayValue: UILabel!
+    
+    @IBAction func clearBtn(_ sender: Any) {
+    }
+    @IBAction func negBtn(_ sender: Any) {
+    }
+    
+    @IBAction func divBtn(_ sender: Any) {
+    }
+    @IBAction func multBtn(_ sender: Any) {
+    }
+    @IBAction func subBtn(_ sender: Any) {
+    }
+    @IBAction func addBtn(_ sender: Any) {
+    }
+    @IBAction func equalBtn(_ sender: Any) {
+    }
+    
+    @IBAction func num0Btn(_ sender: Any) {
+        displayValue.text = "0";
+        print("0 clicked");
+    }
+    @IBAction func num1Btn(_ sender: Any) {
+        displayValue.text = "1";
+        print("1 clicked");
+    }
+    @IBAction func num2Btn(_ sender: Any) {
+        displayValue.text = "2";
+        print("2 clicked");
+    }
+    @IBAction func num3Btn(_ sender: Any) {
+        displayValue.text = "3";
+        print("3 clicked");
+    }
+    @IBAction func num4Btn(_ sender: Any) {
+        displayValue.text = "4";
+        print("4 clicked");
+    }
+    @IBAction func num5Btn(_ sender: Any) {
+        displayValue.text = "5";
+        print("5 clicked");
+    }
+    @IBAction func num6Btn(_ sender: Any) {
+        displayValue.text = "6";
+        print("6 clicked");
+    }
+    @IBAction func num7Btn(_ sender: Any) {
+        displayValue.text = "7";
+        print("7 clicked");
+    }
+    @IBAction func num8Btn(_ sender: Any) {
+        displayValue.text = "8";
+        print("8 clicked");
+    }
+    @IBAction func num9Btn(_ sender: Any) {
+        displayValue.text = "9";
+        print("9 clicked");
+    }
 }
 
