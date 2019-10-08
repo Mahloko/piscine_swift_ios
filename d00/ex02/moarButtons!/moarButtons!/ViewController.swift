155f176b6d5b86ab15ee66babdcc6c28f53bb0d7
Mpho MAHLOKO <mmahloko@c5r1s10.wethinkcode.co.za>
2019-10-08 18:30:38 +0200

    ex02 proper

--- d00/ex02/moarButtons!/moarButtons!/ViewController.swift
+++ d00/ex02/moarButtons!/moarButtons!/ViewController.swift
@@ -20,58 +20,64 @@ class ViewController: UIViewController {
     @IBAction func clearBtn(_ sender: Any) {
     }
     @IBAction func negBtn(_ sender: Any) {
+        print("+/- button clicked");
     }
     
     @IBAction func divBtn(_ sender: Any) {
+        print("/  button clicked");
     }
     @IBAction func multBtn(_ sender: Any) {
+        print("* button clicked");
     }
     @IBAction func subBtn(_ sender: Any) {
+        print("- button clicked");
     }
     @IBAction func addBtn(_ sender: Any) {
+        print("+ button clicked");
     }
     @IBAction func equalBtn(_ sender: Any) {
+        print("= button clicked");
     }
     
     @IBAction func num0Btn(_ sender: Any) {
         displayValue.text = "0";
-        print("0 clicked");
+        print("0 button clicked");
     }
     @IBAction func num1Btn(_ sender: Any) {
         displayValue.text = "1";
-        print("1 clicked");
+        print("1 button clicked");
     }
     @IBAction func num2Btn(_ sender: Any) {
         displayValue.text = "2";
-        print("2 clicked");
+        print("2 button clicked");
     }
     @IBAction func num3Btn(_ sender: Any) {
         displayValue.text = "3";
-        print("3 clicked");
+        print("3 button clicked");
     }
     @IBAction func num4Btn(_ sender: Any) {
         displayValue.text = "4";
-        print("4 clicked");
+        print("4 button clicked");
     }
     @IBAction func num5Btn(_ sender: Any) {
         displayValue.text = "5";
-        print("5 clicked");
+        print("5 button clicked");
     }
     @IBAction func num6Btn(_ sender: Any) {
         displayValue.text = "6";
-        print("6 clicked");
+        print("6 button clicked");
     }
     @IBAction func num7Btn(_ sender: Any) {
         displayValue.text = "7";
-        print("7 clicked");
+        print("7 button clicked");
     }
     @IBAction func num8Btn(_ sender: Any) {
         displayValue.text = "8";
-        print("8 clicked");
+        print("8 button clicked");
     }
     @IBAction func num9Btn(_ sender: Any) {
         displayValue.text = "9";
-        print("9 clicked");
+        print("9 button clicked"); 
     }
 }
 
