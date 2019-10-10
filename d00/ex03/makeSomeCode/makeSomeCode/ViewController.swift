d8642956aec06692dab548126cf9f8769112bd71
Mpho MAHLOKO <mmahloko@c5r1s10.wethinkcode.co.za>
2019-10-10 08:19:52 +0200

    d01 pdf

--- d00/ex03/makeSomeCode/makeSomeCode/ViewController.swift
+++ d00/ex03/makeSomeCode/makeSomeCode/ViewController.swift
@@ -10,11 +10,91 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var labelView: UILabel!
+    var calc = 0
+    var leftExp = 0
+    var opera = 0
     
     @IBAction func numBtnClicked(_ sender: UIButton) {
-        labelView.text = String(sender.tag)
+        if (labelView.text == "0") {
+            calc &*= 10
+            calc &+= sender.tag
+            labelView.text = String(sender.tag)
+        }
+        else {
+            calc &*= 10
+            calc &+= sender.tag
+            labelView.text = String(calc)
+        }
         print(sender.tag)
     }
+    
+    @IBAction func oprationsBtn(_ sender: UIButton) {
+        if (sender.tag >= 12 && sender.tag <= 15) {
+            if (opera != 0) {
+                if (sender.tag == 12) {
+                    leftExp /= calc
+                    calc = 0
+                    print("/ button was clicked")
+                }
+                if (sender.tag == 13) {
+                    leftExp &*= calc
+                    calc = 0
+                    print("* button was clicked")
+                }
+                if (sender.tag == 14) {
+                    leftExp &-= calc
+                    calc = 0
+                    print("- button was clicked")
+                }
+                if (sender.tag == 15) {
+                    leftExp &+= calc
+                    calc = 0
+                    print("+ button was clicked")
+                }
+            }
+            else {
+                if (sender.tag == 12) {
+                    leftExp /= calc
+                    calc = 0
+                    print("/ button was clicked")
+                }
+                if (sender.tag == 13) {
+                    leftExp &*= calc
+                    calc = 0
+                    print("* button was clicked")
+                }
+                if (sender.tag == 14) {
+                    leftExp &-= calc
+                    calc = 0
+                    print("- button was clicked")
+                }
+                if (sender.tag == 15) {
+                    leftExp &+= calc
+                    calc = 0
+                    print("+ button was clicked")
+                }
+            }
+            opera = sender.tag
+            labelView.text = String(leftExp)
+        }
+        if (sender.tag == 10) {
+            calc = 0
+            leftExp = 0
+            opera = 0
+            print("AC button was clicked")
+        }
+        if (sender.tag == 11) {
+            calc *= -1
+            labelView.text = String(calc)
+            print("NEG button was clicked")
+        }
+        if (sender.tag == 16) {
+            leftExp &+= calc
+            labelView.text = String(leftExp)
+            print("= button was clicked")
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
